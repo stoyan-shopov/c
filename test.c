54028d2db80b783d922f10b56fd799a8103a72fd
Stoyan Shopov <stoyan.shopov@gmail.com>
2018-01-08 15:55:48 +0200

    add test file

---
+++ test.c
@@ -0,0 +1,64 @@
+
+
+int x   /**************  ;
+
+
+
+
+	 **************/   
+;
+
+struct s1 { int x; };
+
+struct SSS { int x, y, z; 
+	struct S1
+	{
+		int x;
+		int y[];
+		long double dragon[10][20][30];
+	}
+	****************** vector[][23];
+	float f;
+} str1;
+
+
+int foo()
+{
+	x++;
+	++x;
+	x ++ + ++ x;
+	-- (x & y && z ^ a) ++;
+	x.y;
+	x->y;
+	(x ^ y ^ z ^ y)->a;
+
+	x <<= (y -= (z >>= 5));
+
+	(x < 10) ? x <<= (y -= (z >>= 5)) : 5;
+	-- 3 ++;
+	& 4;
+	"xxxxxxxxxxxx";
+}
+
+struct s
+{
+	int	x;
+	int	y;
+}
+s;
+
+/*
+int voidx;    
+
+int xxx;
+int foo()
+{
+	while (0x1 + 0x1);
+	xxx ++;
+	20[50 + 1] ++;
+	++10[x];
+}
+
+*/
+
+
